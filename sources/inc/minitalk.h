5b54b171a578868b7388408b0ae51c971609aed0
nacho <nacho.mota@gmail.com>
2022-11-06 13:08:43 +0100

    pruebas makefile

---
+++ sources/inc/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 05:55:54 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/11/06 13:04:31 by nmota-bu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+# include <signal.h>
+// # include "../libft/inc/libft.h"
+# include "../libft/inc/ft_printf.h"
+
+#define ERROR_0 1 << 0
+#define ERROR_1 1 << 1
+#define ERROR_2 1 << 2
+#define ERROR_3 1 << 3
+
+#endif
--- sources/src/client/client.c
+++ sources/src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 19:17:20 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/11/04 19:18:17 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/11/06 13:07:13 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,29 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include <stdio.h>
+#include "minitalk.h"
+// #include "../../inc/minitalk.h"
+
+void	ft_error(unsigned char error)
+{
+	if (error & ERROR_0)
+		printf("Error 0\n");
+	if (error & ERROR_1)
+		printf("Error 1\n");
+	if (error & ERROR_2)
+		printf("Error 2\n");
+	if (error & ERROR_3)
+		printf("Error 3\n");
+
+}
 
 int	main(void)
 {
-	printf("Tomate");
+	ft_printf("Tomate\n");
+	ft_error(ERROR_1 | ERROR_3 | ERROR_0);
 
 	return(0);
 }
+
+
+// gcc client.c ../../minitalk ../../libft/libft.a
